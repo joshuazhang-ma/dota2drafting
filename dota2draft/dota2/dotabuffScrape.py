09645c6779e5ab12363c57a3b171d8fce7f58636
Joshua Zhang <joshuazhang.ma@gmail.com>
2020-08-08 15:53:31 -0400

    Formatting

--- dota2draft/dota2/dotabuffScrape.py
+++ dota2draft/dota2/dotabuffScrape.py
@@ -1,9 +1,3 @@
-'''
-Created on Jul 28, 2020
-
-@author: Joshua Zhang
-'''
-
 import requests
 import json
 import os.path
@@ -18,7 +12,7 @@ def getHero():
         request = requests.get("https://api.opendota.com/api/constants/heroes").json()
         matchesDF = pd.DataFrame(request).transpose()
         matchesDF.to_csv('heroList.csv')
-    
+
     return
 
 def getMatches():
@@ -27,7 +21,7 @@ def getMatches():
     
     # Link to the list of matches = "Professional Matches"
     html = "https://api.opendota.com/api/proMatches"
-    
+
     # Get json from html, if the corresponding match data is not already on computer file.
     request = requests.get(html).json()
     print("getMatches - Data successfully received from API.")
@@ -38,7 +32,7 @@ def getMatches():
         matchesDF.to_csv(filename)
     else: # else it exists so append without writing the header
         matchesDF.to_csv(filename, mode='a', header=False)
-    
+
     return
 
 def getDraft(api, match):
@@ -56,10 +50,10 @@ def getDraft(api, match):
         # Write data to json file
         with open(filename, "w") as save_file:
             json.dump(request, save_file)
-    
+
         # with open(filename, "r") as load_file:
         jsonData = request # json.load(load_file)
-    
+
         # With the data loaded, create a DF for picks and bans
         pb = jsonData['draft_timings']
         matchResult = jsonData['radiant_win']
@@ -77,36 +71,26 @@ def getDraft(api, match):
 def addHeroNames():
     draft = pd.read_csv('drafting.csv')
     names = pd.read_csv('heroList.csv', usecols = ['id', 'localized_name'])
-    
+
     combined = draft.merge(names, how = 'inner', left_on = 'hero_id', right_on = 'id')
     combined.to_csv('drafting with names.csv', index = False)
-    
-    return 
+
+    return
 
 def main():
     # Grab hero list
     getHero()
     # Grab some more match data
     getMatches()
-    
+
     # get a unique list of matchID from matchList.csv
     matchList = list(set(pd.read_csv("matchList.csv")['match_id']))
     for match in matchList:
         getDraft(api_key, match)
         time.sleep(1.1)
-      
+
     addHeroNames()
-    
+
     return
 
 main()
-    
-    
-    
-    
-    
-    
-    
-    
-
-
